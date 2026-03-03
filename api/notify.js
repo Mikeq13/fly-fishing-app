9469373f6e54a9dcf58a1fc378d43bf8b7ac4479
Mikeq13 <13jmmq@gmail.com>
2026-03-03 15:13:08 -0700

    Add comment for Twilio SMS notification handler
    Add a comment explaining the purpose of the file.

--- api/notify.js
+++ api/notify.js
@@ -1,3 +1,4 @@
+// Twilio SMS notification handler for Fly Fishing Guru bookings
 export default async function handler(req, res) {
     if (req.method !== 'POST') {
           return res.status(405).json({ error: 'Method not allowed' });
